f16d96825e51e6847977bc07f8874cf56c86ec19
SD614 Apps <sd614.apps@gmail.com>
2023-08-08 22:16:21 +0530

    Added dark mode, theme toggle, profile pic, fonts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+    Container,
+    Box,
+    Heading,
+    Image,
+    useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                p={3}
+                mb={6}
+                align="center"
+            >
                 Hello, I am a full-stack developer based in India!
             </Box>
 
@@ -14,7 +27,31 @@ const Page = () => {
                     </Heading>
                     <p align="center">Web-Dev | Databases | Testing | Data Science | AI</p>
                 </Box>
+                <Box
+                    flexShrink={0}
+                    mt={{base: 4, md: 0}}
+                    ml={{md: 6}}
+                    align="center"
+                >
+                    <Image
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        maxWidth="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        src="/images/sd614apps.jpeg"
+                        alt="Profile Image"
+                    />
+                </Box>
             </Box>
+
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <p>Paragraph</p>
+            </Section>
         </Container>
     )
 }
