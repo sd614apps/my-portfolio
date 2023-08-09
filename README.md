b1ecb47f4cc964cb654fa4969b10a981a2a4161e
SD614 Apps <sd614.apps@gmail.com>
2023-08-10 00:16:33 +0530

    Added Bio section, portfolio link button, and animated the theme toggle button

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # my-portfolio
 
+Portfolio website using Next.js, Chakra UI, Framer Motion, and Three.js
+
 - npm init -y
 - npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion next react react-dom
 - npm i -D eslint eslint-config-next prettier
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import NextLink from 'next/link'
 import {
     Container,
     Box,
     Heading,
     Image,
-    useColorModeValue
+    Link,
+    useColorModeValue,
+    Button
 } from '@chakra-ui/react'
 import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
     return (
@@ -50,7 +56,57 @@ const Page = () => {
                 <Heading as="h3" variant="section-title">
                     Work
                 </Heading>
-                <p>Paragraph</p>
+                <Paragraph>SD614 Apps is a Software Automation Testing Professional with 13+ years of experience of testing Embedded,
+                    Client-Server, Distributed Database Systems, using C, Python, REST APIs, SQL, LDAP, Robot Framework, Docker &
+                    Kubernetes, with an avid interest in Data Science & Machine Learning, currently training LLMs for the most
+                    prestigious AI research firm. Delving into Web Development as a side interest, and building {' '}
+                    <NextLink href="/works/bodhitva">
+                        <Link>Bodhitva</Link>
+                    </NextLink>.
+                </Paragraph>
+                <Box align="center" my={4}>
+                    <NextLink href="/works">
+                        <Button rightIcon={<ChevronRightIcon />} colorScheme='teal'>
+                            Portfolio
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Section>
+
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title">
+                    Bio
+                </Heading>
+                <BioSection>
+                    <BioYear>1984</BioYear>
+                    Born in Silchar, Assam, India.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2005-2009</BioYear>
+                    Completed Bachelor of Engineering in Computer Science from International Institute of
+                    Information Technology (IIIT) Kolkata, West bengal, India.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2010-2017</BioYear>
+                    Worked at Huawei Technologies India Private Limited, Bangalore, Karnataka, India.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2017-2022</BioYear>
+                    Worked at Nokia Networks, Bangalore, Karnataka, India.
+                </BioSection>
+                <BioSection>
+                    <BioYear>2022-Present</BioYear>
+                    Working remotely at Turing, training LLMs for the most prestigious AI research organization.
+                </BioSection>
+            </Section>
+
+            <Section delay={0.3}>
+                <Heading as="h3" variant="section-title">
+                    I ❤️
+                </Heading>
+                <Paragraph>
+                    Music, Driving, Photography.
+                </Paragraph>
             </Section>
         </Container>
     )
