bc3e91e742e7c9d32978db208317c9853b6405c0
SD614 Apps <sd614.apps@gmail.com>
2023-08-10 21:02:49 +0530

    Added portfolio grid containing thumbnails for Bodhitva and Careers Website

---
+++ pages/works.js
@@ -0,0 +1,30 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbBodhitva from '../public/images/works/bodhitva.png'
+import thumbCareersWebsite from '../public/images/works/careers-website.png'
+
+const Works = () => {
+    return (
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Works
+            </Heading>
+
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="bodhitva" title="Bodhitva" thumbnail={thumbBodhitva}>
+                    A website dedicated to enhancing cognitive performance and unlocking the full potential of the human brain.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem id="careers-website" title="CareersWebsite" thumbnail={thumbCareersWebsite}>
+                    Careers at SD614 Apps
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    )
+}
+
+export default Works
